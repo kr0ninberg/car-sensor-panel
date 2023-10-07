416420d0aa435cb87852a5367feaac00cd189041
Iltl <utypostev@gmail.com>
2023-10-08 01:44:43 +0300

    v1.0

--- ms-personal/arduino/com-out-no-inter/com-out-no-inter.ino
+++ ms-personal/arduino/com-out-no-inter/com-out-no-inter.ino
@@ -6,24 +6,16 @@ void setup() {
   pastOther = millis();
 }
 
-int8_t tempInGet(){
-  return 26;
-}
-
-int8_t tempOutGet(){
-  return 13;
-}
-
-uint8_t velocityGet(){
-  static uint8_t pastSpeed = 0;
-  uint8_t res1 = min(random(max(0 - pastSpeed, -10), 11) + pastSpeed, 150);
-  uint8_t res2 = min(random(max(0 - pastSpeed, -10), 11) + pastSpeed, 150);
-  uint8_t res3 = min(random(max(0 - pastSpeed, -10), 11) + pastSpeed, 150);
-  if(abs(60 - res1) < abs(60 - res2)){
-    if(abs(60 - res1) < abs(60 - res3)) return pastSpeed = res1;
-    else return pastSpeed = res3;
-  } else if(abs(60 - res2) < abs(60 - res3)) return pastSpeed = res2;
-  else return pastSpeed = res3;
+float temperatureGet(){
+  static float pastTemperature = 26;
+  float res1 = max(min(random(-100, 100) / 100.0 + pastTemperature, 50), -50);
+  float res2 = max(min(random(-100, 100) / 100.0 + pastTemperature, 50), -50);
+  float res3 = max(min(random(-100, 100) / 100.0 + pastTemperature, 50), -50);
+  if(abs(26 - res1) < abs(26 - res2)){
+    if(abs(26 - res1) < abs(26 - res3)) return pastTemperature = res1;
+    else return pastTemperature = res3;
+  } else if(abs(26 - res2) < abs(26 - res3)) return pastTemperature = res2;
+  else return pastTemperature = res3;
 }
 
 void loop() {
@@ -32,12 +24,7 @@ void loop() {
     //Serial.println(millis());
   }
   if (millis() - pastOther >= 1000){
-    //Serial.print("temp-in ");
-    //Serial.println(tempInGet());
-    //Serial.print("temp-out ");
-    //Serial.println(tempOutGet());
-    //Serial.print("velocity ");
-    Serial.println(velocityGet());
+    Serial.println(temperatureGet());
 
     //Serial.print("time from prev. data:");
     //Serial.println(millis() - pastOther);
